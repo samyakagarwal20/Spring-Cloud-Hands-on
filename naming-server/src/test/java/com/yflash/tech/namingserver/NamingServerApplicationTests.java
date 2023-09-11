8b8519b7d62fd2a358502ee11ec0e280fcf3be3e
samyak <samyakagarwal20@gmail.com>
2023-09-12 02:29:19 +0530

    added base structure of the application

---
+++ naming-server/src/test/java/com/yflash/tech/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yflash.tech.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
