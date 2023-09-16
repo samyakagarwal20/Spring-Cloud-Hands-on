f6576f5687c6754974ea20d9f62bcff89d66df27
samyak <samyakagarwal20@gmail.com>
2023-09-16 13:39:47 +0530

    added dependency for resilience4j-retry

---
+++ currency-exchange-service/src/main/java/com/yflash/tech/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,21 @@
+package com.yflash.tech.currencyexchangeservice.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+    private static final Logger LOGGER = LogManager.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("sample-api")
+
+    public String sampleApi(HttpServletRequest request) {
+        LOGGER.info("Intercepted request --> {}", request.getRequestURI());
+        return "Sample API";
+    }
+
+}
